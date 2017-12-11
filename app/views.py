7336d9f64b400599bd35c2a09e18899943e076ff
ivan_matsiash <ivan.matyash97@gmail.com>
2017-12-11 12:10:24 +0300

    Modify main page

--- app/views.py
+++ app/views.py
@@ -6,21 +6,8 @@ from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}
-    posts = [
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
     return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+                           title='Talons FAMCS')
 
 
 @app.route('/login', methods=['GET', 'POST'])
