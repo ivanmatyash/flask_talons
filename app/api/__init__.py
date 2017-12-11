8b4fcab08a934e58f6c7eb75a8737b88220a0f33
ivan_matsiash <ivan.matyash97@gmail.com>
2017-12-11 12:04:21 +0300

    Add absolute paths to apis

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,1 @@
-import doctor_api
-import role_api
-import poly_api
-import doctor_api
-import people_api
-import spec_api
-import timetable_api
+from app.api import doctor_api, role_api, poly_api, people_api, spec_api, timetable_api
