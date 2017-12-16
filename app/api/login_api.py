f0e5b3b48102f65fa94600c94dde5c83acef41d2
ivan_matsiash <ivan.matyash97@gmail.com>
2017-12-16 13:49:38 +0300

    Add current_user GET endpoint

--- app/api/login_api.py
+++ app/api/login_api.py
@@ -1,6 +1,7 @@
 from flask.ext.login import login_user
 from flask.ext.login import login_required
 from flask.ext.login import logout_user
+from flask.ext.login import current_user
 from flask import request
 from flask import jsonify
 
@@ -28,6 +29,11 @@ def login():
 def logout():
     logout_user()
     return jsonify({'Info': 200, "Message": "Logout success."}), 200
+
+@app.route("/api/user/current_user", methods=["GET"])
+@login_required
+def get_current_user():
+    return jsonify(current_user.get_json())
 
 @app.errorhandler(401)
 def unauth(e):
